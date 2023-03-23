0b0843fa5687282e5b26d7f6a61edb369e3de940
Hammad-001 <hammadfaisal178@gmail.com>
2023-03-24 03:30:33 +0500

    Customer Model added

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __repr__(self):
+        return f'Customer({self.first_name} {self.last_name} {self.email} {self.phone_number} {self.address} {self.city} {self.state} {self.zip_code})'
