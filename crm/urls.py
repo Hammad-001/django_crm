c4eb4cb1bc0758dec9740ab4c969c205d8a29376
Hammad-001 <hammadfaisal178@gmail.com>
2023-03-25 05:55:38 +0500

    Customer CRUD

--- crm/urls.py
+++ crm/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     # path('login/', views.login_user, name="login"),
     path('logout/', views.logout_user, name="logout"),
     path('register/', views.register_user, name="register"),
+    path('add_record/', views.add_record, name="add_record"),
+    path('view_record/<int:customer>', views.view_record, name="view_record"),
+    path('delete_record/<int:customer>', views.delete_record, name="delete_record"),
+    path('update_record/<int:customer>', views.update_record, name="update_record"),
 ]
--- crm/views.py
+++ crm/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
-from .forms import RegistrationForm
+from .forms import RegistrationForm, CustomerForm
+from .models import Customer
 
 
 def home(request):
@@ -14,18 +15,31 @@ def home(request):
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
+
             # if user is authenticated successfully
             login(request, user)
             messages.success(request, "You have been logged in successfully!")
             return redirect('home')
+
         else:
+
             # if credentials are not valid
             messages.success(
                 request, "Username or password is incorrect! Please try again.")
             return redirect('home')
     else:
-        # if user is just accessing the homepage
-        return render(request, 'home.html', {})
+        if request.user.is_authenticated:
+
+            # fetch all customer records
+            records = Customer.objects.all()
+
+            # if user is just accessing the homepage
+            return render(request, 'home.html', {'records': records})
+
+        else:
+
+            # if user is not authenticated and not requesting for login
+            return render(request, 'home.html', {})
 
 
 def logout_user(request):
@@ -34,11 +48,11 @@ def logout_user(request):
         # logout user
         logout(request)
         messages.success(request, "You have been logged out successfully!")
-        return redirect('home')
     else:
         # if user is not logged in then redirect to home
         messages.success(request, "You are not logged in!")
-        redirect('home')
+
+    return redirect('home')
 
 
 def register_user(request):
@@ -48,18 +62,23 @@ def register_user(request):
     if request.method == 'POST':
         # checking if user is submitting data
         form = RegistrationForm(request.POST)
+
         if form.is_valid():
             # if data is valid, then save it
             form.save()
+
             # getting username, password to authenticate and login
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(request, username=username, password=password)
+
             if user is not None:
+
                 # if user authentication succeeded
                 login(request, user)
                 messages.success(request, "You have successfully registered!")
                 return redirect("home")
+
             else:
                 messages.success(request, "Registration failed!!")
                 return redirect("register")
@@ -68,3 +87,119 @@ def register_user(request):
         form = RegistrationForm()
         return render(request, "register.html", {'form': form})
     return render(request, "register.html", {'form': form})
+
+
+def view_record(request, customer):
+    # Check if user is already authenticated
+    if request.user.is_authenticated:
+
+        # if request has customer id
+        if customer is not None:
+
+            try:
+                # getting the record from Customer Model
+                customer = Customer.objects.get(pk=customer)
+                return render(request, 'customer.html', {'customer': customer})
+
+            except Customer.DoesNotExist:
+
+                # if customer ID is not valid
+                messages.success(request, "Requested customer is not a valid customer!")
+                return redirect('home')
+
+        else:
+            messages.success(request, 'Please select a valid customer!')
+
+    else:
+        # if user is not logged in then redirect to home
+        messages.success(request, "You are authorized to view customers!")
+    return redirect('home')
+
+
+def delete_record(request, customer):
+    # Check if user is signed in
+    if request.user.is_authenticated:
+
+        # if request has customer id
+        if customer is not None:
+
+            try:
+                # getting the record from Customer Model
+                customer = Customer.objects.get(pk=customer)
+
+            except Customer.DoesNotExist:
+
+                # if customer ID is not valid
+                messages.success(request, "Requested Record is not a valid customer!")
+                return redirect('home')
+
+            # if Customer found successfully, then delete customer
+            customer.delete()
+            messages.success(request, "Customer Record deleted successfully!")
+
+        else:
+            # if customer ID is not valid
+            messages.success(request, 'Please select a valid customer!')
+
+    else:
+        # if user is not logged in then redirect to home
+        messages.success(request, "You are authorized to delete customers!")
+    return redirect('home')
+
+
+def add_record(request):
+    # Checking if user is logged in
+    if request.user.is_authenticated:
+
+        form = CustomerForm(request.POST or None)
+
+        # checking if user wants to save data of customer
+        if request.method == "POST":
+
+            # checking if customer data is valid
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Customer is successfully registered!")
+                return redirect("home")
+
+        return render(request, 'add_customer.html', {'form': form})
+    else:
+        # if user is not logged in then redirect to home
+        messages.success(request, "You are authorized to delete customers!")
+        return redirect('home')
+
+
+def update_record(request, customer):
+    # Checking if user is logged in
+    if request.user.is_authenticated:
+
+        # if request has customer id
+        if customer is not None:
+
+            try:
+                # getting the record from Customer Model
+                customer = Customer.objects.get(pk=customer)
+
+            except Customer.DoesNotExist:
+
+                # if customer ID is not valid
+                messages.success(request, "Requested Record is not a valid customer!")
+                return redirect('home')
+
+            # if Customer found successfully, then return form of that customer
+            form = CustomerForm(request.POST or None, instance=customer)
+
+            if request.method == "POST":
+                if form.is_valid():
+                    form.save()
+                    messages.success(request, "Customer Record updated successfully!")
+            else:
+                return render(request, 'update_record.html', {'form': form})
+        else:
+            # if customer ID is not valid
+            messages.success(request, 'Please select a valid customer!')
+    else:
+        # if user is not logged in then redirect to home
+        messages.success(request, "You are authorized to delete customers!")
+
+    return redirect('home')
